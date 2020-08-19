db158585be3485e9f40bc4acf188622a0820e568
Leonardo Seiji Teshima <teshimaleo@gmail.com>
2020-08-18 22:13:20 -0300

    segundo commit

--- contador/src/Components/Header/index.js
+++ contador/src/Components/Header/index.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Header = () => {
-    return (
-        <div className="menu">
-          0
-        </div>
-    )
+  const [changeNumber, setChangeNumber] = useState(0)
+
+  const incrementar = () => {
+    setChangeNumber(changeNumber+1);
+  }
+
+  return (
+    <div className="menu">
+      <h1 align="center">{changeNumber}</h1>
+      <button type="button" onClick={incrementar}>Adicionar</button>
+    </div>
+  )
 }
 
 export default Header
