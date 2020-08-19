2d5aaa993d012822911a4949f8231397484ad5a0
Leonardo Seiji Teshima <teshimaleo@gmail.com>
2020-08-18 22:19:12 -0300

    commit 2

--- contador/src/Components/Header/index.js
+++ contador/src/Components/Header/index.js
@@ -3,14 +3,14 @@ import React, { useState } from 'react'
 const Header = () => {
   const [changeNumber, setChangeNumber] = useState(0)
 
-  const incrementar = () => {
-    setChangeNumber(changeNumber+1);
+  const increment = () => {
+    setChangeNumber(changeNumber + 1);
   }
 
   return (
     <div className="menu">
       <h1 align="center">{changeNumber}</h1>
-      <button type="button" onClick={incrementar}>Adicionar</button>
+      <button type="button" onClick={increment}>Adicionar</button>
     </div>
   )
 }
